d422a4a03d3605b6d0a0d04c50d3d5932272939d
Anand Patil <anand.prabhakar.patil@gmail.com>
2009-04-17 17:09:08 +0100

    Added stub for EM backend

--- cov_test/EM.py
+++ cov_test/EM.py
@@ -148,7 +148,7 @@ class EM(MAP):
         return -np.mean(logps)
 
         
-def MCMC_obj(pos,neg,lon,lat,t,cv,cpus,dbname=None,lockdown=False,**kwds):
+def EM_obj(pos,neg,lon,lat,t,cv,cpus,lockdown=False,**kwds):
     """
     Creates an MCMC object around the model for transformed PR at sampled locations.
     """
@@ -156,31 +156,12 @@ def MCMC_obj(pos,neg,lon,lat,t,cv,cpus,dbname=None,lockdown=False,**kwds):
     while True:
         print 'Trying to create model'
         try:
-            if dbname is not None:
-                M = pm.MCMC(make_model(d,lon,lat,t,cv,cpus,lockdown), db='hdf5', dbname=dbname, dbcomplevel=1, dbcomplib='zlib')
-            else:
-                M = pm.MCMC(make_model(d,lon,lat,t,cv,cpus,lockdown))
-            break
+            modstuff = make_model(d,lon,lat,t,cv,cpus,lockdown)
+            S = pm.MCMC(modstuff['cv'].itervalues())
+            M = EM(modstuff, S)
+            # MCMC object should only use Gibbs step methods.
+            S.use_step_method(CovariateStepper, M.covariate_dict, M.m_const, t-2009, M.t_coef, M.M_eval, M.S_eval, M.data)
         except np.linalg.LinAlgError:
             pass
-    M.db._h5file.createArray('/','lon',lon)
-    M.db._h5file.createArray('/','lat',lat)
-    M.db._h5file.createArray('/','t',t)
-    M.db._h5file.createArray('/','data',d)
-    for name, val in cv.iteritems():
-        M.db._h5file.createArray('/',name+'_value',val)
-        
-    # Special Gibbs step method for covariates
-    M.use_step_method(CovariateStepper, M.covariate_dict, M.m_const, t-2009, M.t_coef, M.M_eval, M.S_eval, M.data)
-    # mean_params = [v[0] for v in M.covariate_dict.values()] + [M.m_const, M.t_coef]
-    # M.use_step_method(pm.AdaptiveMetropolis, mean_params, scales=dict(zip(mean_params, [.001]*len(mean_params))), **kwds)
-
-    if lockdown:
-        cov_params = [M.sqrt_ecc, M.inc]
-    else:
-        cov_params = [M.V, M.sqrt_ecc, M.amp, M.scale, M.scale_t, M.t_lim_corr, M.inc]
-    M.use_step_method(pm.AdaptiveMetropolis, cov_params, scales=dict(zip(cov_params, [.001]*len(cov_params))), **kwds)
-
-    S = M.step_method_dict[M.m_const][0]
-
-    return M, S
+    
+    return M
