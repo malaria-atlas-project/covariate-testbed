8a82adb6bf475073ace5a0e185f1a2a0f6bef09d
Anand Patil <anand.prabhakar.patil@gmail.com>
2009-04-23 23:05:34 +0200

    Point estimate covariates branch works kind of

--- cov_test/test_marg.py
+++ cov_test/test_marg.py
@@ -4,8 +4,8 @@ import pymc as pm
 import pylab as pl
 from marginal_model import make_model
 from util import *
-n_data = 9
-n_pred = 1
+n_data = 90
+n_pred = 10
 
 # make_model(d,lon,lat,t,covariate_values)
 names = ['rain','temp','ndvi']
@@ -51,6 +51,8 @@ pl.rec2csv(ra_pred,'test_pred.csv')
 
 prior_var=1e20
 N = pm.MAP(make_model(transform_bin_data(pos[:n_data],neg[:n_data]),lon[:n_data],lat[:n_data],t[:n_data],cv_data,2,prior_var))
+N.fit()
+print N.C.value.params
 
 # C1 = N.T.parents['C'].value(N.logp_mesh, N.logp_mesh)+N.V.value*np.eye(N.logp_mesh.shape[0]) + N.u.T*N.u*prior_var
 # C2 = N.marg_T.value.I
